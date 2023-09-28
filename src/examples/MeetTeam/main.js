e23cb8cffa93d796931e52976dad9da1360784e1
Jacob Vicente López <jacobvl@gmail.com>
2023-09-28 13:13:27 +0200

    Refactor MeetTeam

--- src/examples/MeetTeam/main.js
+++ src/examples/MeetTeam/main.js
@@ -1,63 +1,105 @@
-import { render, data, addEvent } from '../../rathole'
+import { render, data, addEvent, stores } from '../../rathole'
 import './format.css'
 
 export default function MeetTeam(app) {
-    /**Url base de llamada de datos */
+    /**Stores */
+    const socialState = stores.createStore('socialStore')
+    stores.subscribe('socialStore', (data) => {
+        console.log('cambios en socialStore:', data)
+    })
+    //stores.updateStore ('socialStore', {viewIcons: false});
+
+    //Url base de llamada de datos
     data.baseUrl = ''
+    //Listado de datos
+    let dataList
 
     /**Cards */
 
-    const socialIcons = () => `
-  <div class="footer-social-icons">
+    const socialIcons = (id) => {
+        const visibility = stores.getStore('socialStore')
+        return `
+  <div class="footer-social-icons" style="display:${
+      visibility[id] ? 'block' : 'none'
+  }">
     <ul class="social-icons">
           <li><a href="#" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
           <li><a href="#" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
           <li><a href="#" class="social-icon"> <i class="fa fa-rss"></i></a></li>
           <li><a href="#" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
           <li><a href="#" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
-          <li><a href="#" class="social-icon"> <i class="fa fa-github"></i></a></li>
     </ul>
   </div>
   `
+    }
 
-    const column = ({ name, photo, title, description, contact }) =>
-        `
+    const column = ({ id, name, photo, title, description, contact }) => {
+        const visibility = stores.getStore('socialStore')
+        return `
   <div class="column_meet">
     <div class="card_meet">
       <img src="${photo}" alt="${name}" style="width:100%">
       <div class="container_meet">
         <h5>${name}</h5>
         <p class="title_meet">${title}</p>
-        ${socialIcons()}
+        ${socialIcons(`viewIcons_${id}`)}
         <p>${description}</p>
         <p>${contact}</p>
-        <p><button class="button_meet">Contact</button></p>
+        <p><button id=${id} class="button_meet">${
+            visibility[`viewIcons_${id}`] ? 'Close' : 'Contact'
+        }</button></p>
       </div>
     </div>
   </div>
     `
+    }
+
+    // Div contenedor de las cards
+    const PageLayout = (data) => `
+     <div class="row_meet">
+     ${data.map((list) => column(list)).join('')}
+     </div>
+     `
 
     const LoadData = `<div class="pre_colum_meet">...</div>`
     const ButtonCard = `<button id="load-card">Load cards</button>`
 
+    /**Mostrar contacto */
+    function getSocial(e) {
+        console.log('boton:', e.srcElement.id)
+        const id = `viewIcons_${e.srcElement.id}`
+        const getStore = stores.getStore('socialStore')
+        const name = {}
+        name[id] = getStore[id] ? false : true
+        stores.updateStore('socialStore', name)
+        renderDOM([PageLayout(dataList), ButtonCard], app, dataList, id)
+    }
+
+    /**Render & events */
     function renderDOM(...compo) {
-        /**Renders */
+        //Renders
         render(...compo)
         addEvent('load-card', 'click', fetchData)
+
+        //Si llega el "dataList", generamos eventos y guardamos en los stores,para controlar las cards
+        if (compo[2] !== undefined) {
+            const getStore = stores.getStore('socialStore')
+            const objNames = {}
+            compo[2].map((list) => {
+                const id = `viewIcons_${list.id}`
+                objNames[id] = getStore[id] ? getStore[id] : false
+                addEvent(list.id, 'click', getSocial)
+            })
+            stores.updateStore('socialStore', objNames)
+        }
     }
 
     /**Llamada a los datos */
     async function fetchData() {
         console.log('LoadData...')
-        const dataList = await data.get('team.json')
+        dataList = await data.get('team.json')
 
-        const PageLayout = `
-        <div class="row_meet">
-        ${dataList.map((list) => column(list)).join('')}
-        </div>
-        `
-
-        renderDOM([PageLayout, ButtonCard], app)
+        renderDOM([PageLayout(dataList), ButtonCard], app, dataList)
     }
 
     /*Renderizamos elementos base */
